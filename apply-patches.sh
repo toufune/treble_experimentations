35482f3afe25adb9727fdd605770067a96b5a726
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2021-08-08 02:24:17 +0000

    Do not check for already applied patches
    Already applied patches should be identified and discarded.
    Also, the check isn't 100% accurate.

--- apply-patches.sh
+++ apply-patches.sh
@@ -10,11 +10,6 @@ for project in $(cd $patches/patches; echo *);do
 	pushd $p
 	git clean -fdx; git reset --hard
 	for patch in $patches/patches/$project/*.patch;do
-		#Check if patch is already applied
-		if patch -f -p1 --dry-run -R < $patch > /dev/null;then
-			continue
-		fi
-
 		if git apply --check $patch;then
 			git am $patch
 		elif patch -f -p1 --dry-run < $patch > /dev/null;then
