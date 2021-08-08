16a6e1449b822ed943c937dcb742d88cbd74392a
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2021-08-08 02:25:07 +0000

    Change path for patches
    Prepare for unified patches repo

--- apply-patches.sh
+++ apply-patches.sh
@@ -4,12 +4,12 @@ set -e
 
 patches="$(readlink -f -- $1)"
 
-for project in $(cd $patches/patches; echo *);do
+for project in $(cd $patches; echo *);do
 	p="$(tr _ / <<<$project |sed -e 's;platform/;;g')"
 	[ "$p" == build ] && p=build/make
 	pushd $p
 	git clean -fdx; git reset --hard
-	for patch in $patches/patches/$project/*.patch;do
+	for patch in $patches/$project/*.patch;do
 		if git apply --check $patch;then
 			git am $patch
 		elif patch -f -p1 --dry-run < $patch > /dev/null;then
